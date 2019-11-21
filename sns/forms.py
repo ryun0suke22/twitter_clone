ade5016115443d8e64fbcc592ec8efe861317e25
ryun0suke22 <jhan.k.police@gmail.com>
2019-11-21 10:17:15 +0900

    extract group messages

--- sns/forms.py
+++ sns/forms.py
@@ -1,16 +1,25 @@
 from django import forms
-from .models import Message
+from .models import Message, Group
 from django.contrib.auth.models import User
 
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ['owner', 'content']
+#class MessageForm(forms.ModelForm):
+#    class Meta:
+#        model = Message
+#        fields = ['owner', 'content']
+#        #fields = ['owner', 'group', 'content']
 
 class PostForm(forms.Form):
     content = forms.CharField(max_length=500, \
                               widget=forms.Textarea)
+    #group = forms.CharField(max_length=500, \
+    #                          widget=forms.Textarea)
 
     def __init__(self, user, *args, **kwargs):
         super(PostForm, self).__init__(*args, **kwargs)
-        #public = User.objects.filter(username='public').first()
+        ### added
+        self.fields['groups'] = forms.ChoiceField(
+            choices=[('-', '-')] + [(item.title, item.title) \
+            for item in Group.objects. \
+            #filter(owner__in=[user, public])],
+            filter(owner__in=[user])],
+        )
--- sns/models.py
+++ sns/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, \
                               related_name='message_onwner')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, default='public')
     content = models.TextField(max_length=1000)
     pub_date = models.DateTimeField(auto_now_add=True)
 
@@ -12,3 +13,11 @@ class Message(models.Model):
 
     class Meta:
         ordering = ('-pub_date', )
+
+class Group(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, \
+                              related_name='group_owner')
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
